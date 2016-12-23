290fe3ce54a54bbde75502ab738067c2c8e3e7cb
Rendy Marojahan <rendy.marojahan@gmail.com>
2016-12-23 18:21:53 +0700

    Add New

---
+++ www/js/app.js
@@ -0,0 +1,94 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.rendy', {
+    url: '/rendy',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/rendy.html',
+        controller: 'rendyCtrl'
+      }
+    }
+  })
+
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html'
+      }
+    }
+  })
+
+  .state('app.project', {
+    url: '/project',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/project.html'
+      }
+    }
+  })
+    
+
+  .state('app.blog', {
+    url: '/blog',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/blog.html'
+      }
+    }
+  })
+
+  .state('app.carier', {
+    url: '/carier',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/carier.html'
+      }
+    }
+  })
+
+  .state('app.contact', {
+    url: '/contact',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/contact.html'
+      }
+    }
+  })
+
+  ;
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/rendy');
+});
